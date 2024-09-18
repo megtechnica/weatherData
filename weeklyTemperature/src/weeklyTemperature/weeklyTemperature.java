afc3f2c1daa831d2ab0d78e9d124c06aad2680fd
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2024-09-18 13:45:28 -0600

    Add files via upload

--- weeklyTemperature/src/weeklyTemperature/weeklyTemperature.java
+++ weeklyTemperature/src/weeklyTemperature/weeklyTemperature.java
@@ -8,7 +8,7 @@ public class weeklyTemperature {
 	      // instantiate variables
 	      String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	      double[] temperatures = {78.9, 75.6, 73.4, 68.9, 72.4, 74.5, 73.4};
-	      String sentinel = "n";
+	      String sentinel = "y";
 	      
 	      int i;
 	      double dailyTemperature;
@@ -21,64 +21,53 @@ public class weeklyTemperature {
 	      System.out.println("OR enter 'Week' and see the whole week. ");
 	      
 	      
-	      while (sentinel.toLowerCase() != "y") {
+	      while (sentinel.equalsIgnoreCase("y")) {
 	    	  System.out.println("What would you like to do? ");
 	          String answer = scnr.nextLine();
 	          // if week is entered, display each day of the week
 	          // and the temperature entered as well as the average
-	          if (answer.toLowerCase() == "week") {
+	          if (answer.equalsIgnoreCase("week")) {
 	        	  double sum = 0;
-	        	  for (i=0; i <= days.length; i ++) {
+	        	  for (i=0; i < days.length; i ++) {
 	        		  System.out.println(days[i] + ": " + temperatures[i] + " degrees farenheit. ");
 	        		  sum = temperatures[i] + sum;
 	        		  }
 	        	  double averageTemperature = sum / temperatures.length;
-	        	  System.out.println("Average temperature: " + sum + " degrees farenheit. ");
+	        	  System.out.println("Average temperature: " + averageTemperature + " degrees farenheit. ");
 	        	  }
-	          else if (answer.toLowerCase() == "monday") {
+	          else if (answer.equalsIgnoreCase("monday")) {
 	        	  System.out.println("The temperature on " + answer +" was " + temperatures[1]);
 	          }
-//	          else if (answer.toLowerCase() == "tuesday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[2] = dailyTemperature;
-//	          }
-//	          else if (answer.toLowerCase() == "wednesday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[3] = dailyTemperature;
-//	          }
-//	          else if (answer.toLowerCase() == "thursday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[4] = dailyTemperature;
-//	          }
-//	          else if (answer.toLowerCase() == "friday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[5] = dailyTemperature;
-//	          }
-//	          else if (answer.toLowerCase() == "saturday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[6] = dailyTemperature;
-//	          }
-//	          else if (answer.toLowerCase() == "sunday") {
-//	        	  System.out.println("Enter the temperature on " + answer +": ");
-//	        	  dailyTemperature = scnr.nextDouble();
-//	        	  temperatures[0] = dailyTemperature;
-//	        	  System.out.println("The temperature on " + answer +" was " + dailyTemperature);
-//	          }
-//	          else {
-//	        	  // error message upon receiving erroneous input
-//	        	  System.out.println("Uh oh!  You entered something unexpected!");
-//	        	  System.out.println("You can try again, just make sure to enter ");
-//	        	  System.out.println("either the day of the week you want to see ");
-//	        	  System.out.println("weather for OR just 'week' for data from the ");
-//	        	  System.out.println("entire week! ");
-//	          }
-//	          System.out.println("Would you like to enter something else? (y or n) ");
-//	          sentinel = scnr.nextLine();
+	          else if (answer.equalsIgnoreCase("tuesday")) {
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[2]);
+	          }
+	          else if (answer.equalsIgnoreCase("wednesday")) {
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[3]);
+	          }
+	          else if (answer.equalsIgnoreCase("thursday")){
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[4]);
+	          }
+	          else if (answer.equalsIgnoreCase("friday")) {
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[5]);
+	          }
+	          else if (answer.equalsIgnoreCase("saturday")) {
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[6]);
+	          }
+	          else if (answer.equalsIgnoreCase("sunday")) {
+	        	  System.out.println("The temperature on " + answer +" was " + temperatures[0]);
+	          }
+	          else {
+	        	  // error message upon receiving erroneous input
+	        	  System.out.println("Uh oh!  You entered something unexpected!");
+	        	  System.out.println("You can try again, just make sure to enter ");
+	        	  System.out.println("either the day of the week you want to see ");
+	        	  System.out.println("weather for OR just 'week' for data from the ");
+	        	  System.out.println("entire week! ");
+	          }
+	          System.out.println("Would you like to enter something else? (y or n) ");
+	          sentinel = scnr.nextLine();
 	}
+	System.out.println("Goodbye!");
+	return;
 }
 }
